b50382f254323e074878ccc2ad326713a02c7ab0
20Federico <fefe.molino@gmail.com>
2021-11-03 18:34:03 +0100

    struttura html/css completata

---
+++ js/main.js
@@ -0,0 +1,16 @@
+'use strict'
+Vue.config.devtools = true;
+
+window.addEventListener('DOMContentLoaded', function () {
+  new Vue({
+      el: '',
+      data: {
+        username: '',
+      },
+      methods: {
+        function () {
+          
+        },
+      },
+  });
+})
