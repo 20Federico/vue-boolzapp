e4644d9fae7b85a637c99e629caf709a714980ff
20Federico <fefe.molino@gmail.com>
2021-11-05 23:54:59 +0100

    added light background to active contact in contact list

--- js/main.js
+++ js/main.js
@@ -9,84 +9,99 @@ window.addEventListener('DOMContentLoaded', function () {
           {
             name: 'Michele',
             profileImgSrc: 'img/avatar_1.jpg',
+            active: false,
             messages: [
               {
                 date: '10/01/2020 15:30:55',
                 text: 'Hai portato a spasso il cane?',
-                status: 'sent'
+                status: 'sent',
+                infoOpen: false
               },
               {
                 date: '10/01/2020 15:50:00',
                 text: 'Ricordati di dargli da mangiare',
-                status: 'sent'
+                status: 'sent',
+                infoOpen: false
               },
               {
                 date: '10/01/2020 16:15:22',
                 text: 'Tutto fatto!',
-                status: 'received' 
+                status: 'received',
+                infoOpen: false 
               }
             ],
           },
           {
             name: 'Fabio',
             profileImgSrc: 'img/avatar_2.jpg',
+            active: false,
             messages: [
               {
                 date: '20/03/2020 16:30:00',
                 text: 'Ciao come stai?',
-                status: 'sent'
+                status: 'sent',
+                infoOpen: false 
               },
               {
                 date: '20/03/2020 16:30:55',
                 text: 'Bene grazie! Stasera ci vediamo?',
-                status: 'received'
+                status: 'received',
+                infoOpen: false 
               },
               {
                 date: '20/03/2020 16:35:00',
                 text: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                status: 'sent'
+                status: 'sent',
+                infoOpen: false 
               }
             ],
           },
           {
             name: 'Samuele',
             profileImgSrc: 'img/avatar_3.jpg',
+            active: false,
             messages: [
               {
                 date: '28/03/2020 10:10:40',
                 text: 'La Marianna va in campagna',
-                status: 'received'
+                status: 'received',
+                infoOpen: false 
               },
               {
                 date: '28/03/2020 10:20:10',
                 text: 'Sicuro di non aver sbagliato chat?',
-                status: 'sent'
+                status: 'sent',
+                infoOpen: false 
               },
               {
                 date: '28/03/2020 16:15:22',
                 text: 'Ah scusa!',
-                status: 'received'
+                status: 'received',
+                infoOpen: false 
               }
             ],
           },
           {
             name: 'Luisa',
             profileImgSrc: 'img/avatar_io.jpg',
+            active: false,
             messages: [
               {
                 date: '10/01/2020 15:30:55',
                 text: 'Lo sai che ha aperto una nuova pizzeria?',
-                status: 'sent'
+                status: 'sent',
+                infoOpen: false 
               },
               {
                 date: '10/01/2020 15:50:00',
                 text: 'Si, ma preferirei andare al cinema',
-                status: 'received'
+                status: 'received',
+                infoOpen: false 
               }
             ],
           },
         ],
-        findContact: "",
+        toFindContact: "",
         filteredContactList: [],
         activeContact: "",
         newMsg: "",
@@ -103,19 +118,26 @@ window.addEventListener('DOMContentLoaded', function () {
           return(lastMsg);
         },
 
-        showFilteredContacts(findContact) {
+        showFilteredContacts(toFindContact) {
           this.filteredContactList = this.contactList.filter(contact => {
-            return contact.name.toLowerCase().includes(findContact.toLowerCase().trim())
+            return contact.name.toLowerCase().includes(toFindContact.toLowerCase().trim())
           });
         },
         
         openChat(contact) {
           this.activeContact = contact;
+          for (const key in this.contactList) {
+            this.contactList[key].active = false
+          }
+          this.activeContact.active = true;
         },
 
-        // onOpenOptions(event) {
-        //   console.log(event.target.tagName);
-        // },
+        onOpenOptions(message) {
+          this.activeContact['messages'].forEach(element => {
+            element.infoOpen = false;
+          });
+          message.infoOpen = true;
+        },
         
         onEnterAddNewMsg() {
           if (this.newMsg === "") {
@@ -124,7 +146,8 @@ window.addEventListener('DOMContentLoaded', function () {
             this.activeContact.messages.push({
               date: '10/01/2020 15:30:55',
               text: this.newMsg,
-              status: 'sent'
+              status: 'sent',
+              infoOpen: false 
             })
             this.newMsg = "";
             
@@ -132,7 +155,8 @@ window.addEventListener('DOMContentLoaded', function () {
               this.activeContact.messages.push({
                 date: '10/01/2020 15:30:55',
                 text: 'ok',
-                status: 'received'
+                status: 'received',
+                infoOpen: false 
               });
             }, 1000)
           };
@@ -141,6 +165,7 @@ window.addEventListener('DOMContentLoaded', function () {
       
       mounted () {
         this.activeContact = this.contactList[0];  
+        this.activeContact.active = true;
         this.filteredContactList = this.contactList
       },
     });
